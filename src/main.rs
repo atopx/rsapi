39c2124b2d28b9ccac4942c4ae7e24b320dd8244
atopx <3940422@qq.com>
2025-04-13 02:20:06 +0800

    update router

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-use std::time::Duration;
-
-use axum::Router;
-use axum::body::Body;
-use axum::http::Request;
-use axum::http::Response;
-use axum::middleware;
-use tower_http::classify::ServerErrorsFailureClass;
-use tracing::Span;
 use tracing_subscriber::EnvFilter;
 use tracing_subscriber::fmt::time::OffsetTime;
 
@@ -40,26 +31,7 @@ async fn main() {
 
     schedule::start(&config.crontab).await.expect("scheduler start failed");
 
-    let trace_layer = tower_http::trace::TraceLayer::new_for_http()
-        .make_span_with(
-            |_: &Request<Body>| tracing::info_span!("API", trace_id = %uuid::Uuid::new_v4().to_string()),
-        )
-        .on_request(|req: &Request<Body>, span: &Span| {
-            tracing::debug!(parent: span, method = %req.method(), uri = %req.uri(), "request");
-        })
-        .on_response(|resp: &Response<Body>, latency: Duration, span: &Span| {
-            tracing::debug!( parent: span, latency = ?latency, status = %resp.status(), "response");
-        })
-        .on_failure(|error: ServerErrorsFailureClass, latency: Duration, span: &Span| {
-            tracing::error!(parent: span, latency = ?latency, error = %error, "handler failure");
-        });
-
-    let app = Router::new()
-        .nest("/api", router::auth_routes())
-        .nest("/api", router::no_auth_routes())
-        .layer(tower_http::cors::CorsLayer::permissive())
-        .layer(trace_layer)
-        .layer(middleware::from_fn(middle::error_handler));
+    let app = router::new();
 
     let listener = tokio::net::TcpListener::bind(&config.server.listen_addr).await.unwrap();
     tracing::info!(
--- src/router.rs
+++ src/router.rs
@@ -1,13 +1,42 @@
+use std::time::Duration;
+
 use axum::Router;
+use axum::body::Body;
+use axum::http::Request;
+use axum::http::Response;
 use axum::middleware;
 use axum::routing::get;
 use axum::routing::post;
+use tower_http::classify::ServerErrorsFailureClass;
 
-use crate::middle::authorization;
+use crate::middle;
 use crate::service;
 
 pub fn auth_routes() -> Router {
-    Router::new().route("/claims", get(service::claims)).layer(middleware::from_fn(authorization))
+    Router::new().route("/claims", get(service::claims)).layer(middleware::from_fn(middle::authorization))
 }
 
 pub fn no_auth_routes() -> Router { Router::new().route("/login", post(service::login)) }
+
+pub fn new() -> Router {
+    let trace_layer = tower_http::trace::TraceLayer::new_for_http()
+        .make_span_with(
+            |_: &Request<Body>| tracing::info_span!("API", trace_id = %uuid::Uuid::new_v4().to_string()),
+        )
+        .on_request(|req: &Request<Body>, span: &tracing::Span| {
+            tracing::debug!(parent: span, method = %req.method(), uri = %req.uri(), "request");
+        })
+        .on_response(|resp: &Response<Body>, latency: Duration, span: &tracing::Span| {
+            tracing::debug!( parent: span, latency = ?latency, status = %resp.status(), "response");
+        })
+        .on_failure(|error: ServerErrorsFailureClass, latency: Duration, span: &tracing::Span| {
+            tracing::error!(parent: span, latency = ?latency, error = %error, "handler failure");
+        });
+
+    Router::new()
+        .nest("/api", auth_routes())
+        .nest("/api", no_auth_routes())
+        .layer(tower_http::cors::CorsLayer::permissive())
+        .layer(trace_layer)
+        .layer(middleware::from_fn(middle::error_handler))
+}
